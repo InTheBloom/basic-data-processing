efdf826dfa213ab5da68251494b2fea58926a0de
InTheBloom <nato.rider.smm2@gmail.com>
2024-11-12 21:36:43 +0900

    mainの作成を開始

--- builder.py
+++ builder.py
@@ -1,7 +1,10 @@
 from builder_utils import Problem
 import pathlib
+import os
+import sys
+import datetime
 
-def main ():
+def test ():
     problem = Problem(pathlib.Path("./resources/problem1"))
     print(problem.get_title())
     print(problem.get_problem_statement())
@@ -15,6 +18,26 @@ def main ():
     print(problem.get_answer_text())
 
     print(problem.get_judge_code())
+
+RESOURCE_DIR_NAME = "resources"
+TARGET_DIR_NAME = "docs"
+
+def main ():
+    current_dir = pathlib.Path.cwd()
+    resource_dir = current_dir.joinpath(RESOURCE_DIR_NAME)
+
+    # リソースディレクトリの検索
+    if not resource_dir.exists():
+        print(f"[ERROR] {resource_dir} is not found.", file = sys.stderr)
+        sys.exit(1)
+
+    if not resource_dir.is_dir():
+        print(f"[ERROR] {resource_dir} must be a directory.", file = sys.stderr)
+        sys.exit(1)
+
+    print("[INFO] Resource directory is found.")
+
+    # リソースディレクトリの全ディレクトリをProblemディレクトリだとみなして構築
 
 if __name__ == "__main__":
     main()
